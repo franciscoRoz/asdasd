4b464c49006dcc943fb8d1ef09a1c361d972c330
Francisco <francisco.rozas@outlook.com>
2020-12-22 00:09:03 -0300

    version11

--- tests/test.js
+++ tests/test.js
@@ -1,36 +1,19 @@
 'use strict';
-/*let inicio_BP=()=>{
-  
-const child_process = require('child_process');
 
-let comando = child_process.spawn('cmd', ['/c', 'C:\\Users\\Fran-Nico\\Desktop\\test\\nuevo2.bat']);
+var test = require('unit.js');
+var index = require('../index.js');
 
-comando.stdout.on('data', function(datos){
-    console.log('Salida:');
-    console.log(datos.toString());
-});
-
-comando.stderr.on('data', function(datos){
-    console.log('Error: ', datos);
-})
-
-comando.on('exit', function(codigo){
-    console.log('El proceso ha finalizado con el códig:', codigo);
-});
-
-}
-inicio_BP();*/
 describe('Tests index', function() {
   it('verifies successful response', function(done) {
-    
+    index.get({ /* event */ }, { /* context */ }, (err, result) => {
       try {
-        
+        test.number(result.statusCode).is(200);
+        test.string(result.body).contains('Congratulations');
+        test.value(result).hasHeader('content-type', 'text/html');
         done();
       } catch(error) {
         done(error);
       }
- 
+    });
   });
 });
-
-
